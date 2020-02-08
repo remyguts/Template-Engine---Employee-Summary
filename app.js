847ed8bc803c87053bd2295bf655373a620c0b65
Remy Guts <remyguts@gmail.com>
2020-02-07 22:34:27 -0800

    edited out un neeeded questions

--- app.js
+++ app.js
@@ -2,3 +2,19 @@
 
 const inquirer = require("inquirer");
 const prompt = require("prompt");
+const fs = require("fs");
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?"
+    },
+    {
+      type: "input",
+      name: "name",
+      message: "What is your posiiton?"
+    }
+  ]);
+}
